aaa3cf0a602d74d177d1c902bbd799cfd6a62b94
rafimd422 <rafimf2222@gmail.com>
2023-11-11 16:37:21 +0600

    setup a carousel on our home page by using react resposive careusel

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Footer from './../Pages/Shared/Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import NavBar from './../Pages/Shared/NavBar/NavBar';
 
 const Main = () => {
   return (
-    <div>
+    <div className='w-full'>
+        <NavBar />
       <Outlet/>
       <Footer />
     </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Banner from '../Banner/Banner'
 
 const Home = () => {
   return (
-    <div>
-      This is home
+    <div className='top-0 relative'>
+      <Banner/>
     </div>
   )
 }
