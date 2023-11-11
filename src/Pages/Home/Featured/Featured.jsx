295d57db79969d57196af61ea433ba1d979e7593
rafimd422 <rafimf2222@gmail.com>
2023-11-12 01:57:20 +0600

    complated featured section

---
+++ src/Pages/Home/Featured/Featured.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import SectionTitle from "./../../../Components/SectionTitle/SectionTitle";
+import featuredImg from "../../../assets/home/featured.jpg";
+import "./Featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured-item bg-fixed bg-cover my-20 text-white">
+    <div className="w-full py-8 h-full backdrop-brightness-50">
+      <SectionTitle subHeading={"Cheak it Out"} heading={"Featured Item"} />
+      <div className="flex justify-evenly gap-3 items-center py-12 md:px-26 sm:px-8 px-20 sm:flex-nowrap flex-wrap">
+        <div className="md:w-[50%]">
+          <img src={featuredImg} alt="featured image" />
+        </div>
+
+        <div className="md:ml-10 md:w-[50%] space-y-1">
+          <p>Aug 20, 2029</p>
+          <p className="uppercase">Where can i get some?</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda,
+            veritatis quis? Porro in quo odio numquam, voluptatibus quam
+            perferendis veritatis sequi unde a nulla et dolores doloremque
+            laboriosam eaque possimus eveniet quisquam facilis animi alias
+            similique impedit! Voluptatibus ad quisquam voluptas deserunt, dolor
+            placeat facilis sit ab aspernatur sed temporibus.
+          </p>
+          <button className="btn btn-outline">Order Now</button>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../Banner/Banner'
 import Category from './../Category/Category';
 import PopulerMenu from './PopulerMenu/PopulerMenu';
+import Featured from './Featured/Featured';
 
 const Home = () => {
   return (
@@ -10,6 +11,8 @@ const Home = () => {
       <Category />
 
       <PopulerMenu />
+
+      <Featured />
     </div>
   )
 }
